addd693c28d8065b64a3d4fb180cf5110969cb8c
DmitrySkvortsov78 <scvortsov78@yandex.ru>
2025-01-04 18:33:19 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+
+heading = input("Заголовок 1: ")
+heading2 = input("Заголовок 2: ")
+heading3 = input("Заголовок 3: ")
+headings = [heading, heading2, heading3]
+print("Заголовок 1:", headings[0])
+print("Заголовок 2:", headings[1])
+print("Заголовок 3:", headings[2])
